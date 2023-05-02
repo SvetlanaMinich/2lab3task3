23e8b7ba6bc0c02b1be995c79011640357173d06
SvetlanaMinich <imsvetam@mail.ru>
2023-05-02 06:34:31 +0300

    done task3

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -16,6 +16,8 @@ MainWindow::~MainWindow()
 
 Vector<int> v(14);
 Vector<int> b(10);
+int temp = 0;
+QString el = "";
 
 void swap(int* x, int* y)
 {
@@ -41,10 +43,10 @@ void sort(Vector<int>& arr)
 
 void MainWindow::on_push_back_clicked()
 {
-    QString el= ui->lineEdit->text();
+    el= ui->lineEdit->text();
        v.push_back(el.toInt());
        qDebug()<<el;
-       el="";
+//       el="";
        ui->lineEdit->clear();
 
 }
@@ -52,15 +54,22 @@ void MainWindow::on_push_back_clicked()
 
 void MainWindow::on_pushButton_clicked()
 {
-    for (int i = 0; i < v.size(); i += 3)
+    for (int i = temp; i < v.size(); i += 3)
            {
+               temp+=3;
                int median;
                if (i + 2 < v.size())
                {
                    Vector<int> tmp;
                    tmp.push_back(v[i]); tmp.push_back(v[i + 1]); tmp.push_back(v[i + 2]);
+                   qDebug()<<QString::number(v[i]);
+                   qDebug()<<QString::number(v[i+1]);
+                   qDebug()<<QString::number(v[i+2]);
 
                    sort(tmp);
+                   qDebug()<<tmp[0];
+                   qDebug()<<tmp[1];
+                   qDebug()<<tmp[2];
                    median = tmp[1];
                }
                else if (i + 1 < v.size())
@@ -74,18 +83,17 @@ void MainWindow::on_pushButton_clicked()
 
                b.push_back(median);
            }
-
            QString vec="";
 
-           for (int i : b)
+           for (int i=0; i<b.size(); i++)
            {
-              vec+=QString::number(i);
+              vec+=QString::number(b[i]);
               vec+=" ";
            }
 
            QMessageBox::information(this,"Вектор из медиан",vec);
-           v.clear();
-           b.clear();
+//           v.clear();
+//           b.clear();
 }
 
 
@@ -98,7 +106,7 @@ void MainWindow::on_pushButton_2_clicked()
        vec+=QString::number(i);
        vec+=" ";
     }
-
+    vec += el;
     QMessageBox::information(this,"Вектор ",vec);
 }
 
